de6df87db0d5d5005217660fabf7febc16185905
arthur <arthur@arthur-EasyNote-TS11HR>
2015-01-20 22:46:18 +0100

    Deleting Person, no need anymore

--- HelloWorld/src/mypackage/Ausprobieren.java
+++ HelloWorld/src/mypackage/Ausprobieren.java
@@ -3,9 +3,7 @@ package mypackage;
 public class Ausprobieren {
 
 	public static void main(String[] args) {
-		Person ich = new Person();
-		ich.jahrgang = 99;
-		System.out.println(ich.jahrgang);
+
 	}
 
 }
