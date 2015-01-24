b82801e6ea3ed0ba42e63f6f969a49451d46084a
arthur <arthur@arthur-EasyNote-TS11HR>
2015-01-24 22:08:58 +0100

    Added ojdbcDriver changing to oracle again!

--- HelloWorld/src/mypackage/Connector.java
+++ HelloWorld/src/mypackage/Connector.java
@@ -7,15 +7,17 @@ import java.sql.Statement;
 
 public class Connector {
 
-	private static String url = "jdbc:mysql://sql3.freemysqlhosting.net";
-	private static String acc = "sql364370";
-	private static String pw = "vD5!dE1%";
+
 	private static Connection connect = null;
 	private Statement statement = null;
 	private ResultSet resultset = null;
 
 	public static void main(String[] args) {
 		try {
+			String url = "jdbc:mysql://sql3.freemysqlhosting.net:3306/sql364370";
+			String acc = "sql364370";
+			String pw = "vD5!dE1%";
+			
 			System.out.println("trolololo.");
 			Class.forName("com.mysql.jdbc.Driver");
 			connect = DriverManager.getConnection(url,acc,pw);
